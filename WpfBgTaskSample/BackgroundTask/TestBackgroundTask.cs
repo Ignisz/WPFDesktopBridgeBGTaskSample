ec2bfac1ca25286b752cc7b9526ea380db028108
illia.syvolovskyi@dct.ua <illia.syvolovskyi@dct.ua>
2020-10-15 15:01:58 +0300

    Initial commit

---
+++ WpfBgTaskSample/BackgroundTask/TestBackgroundTask.cs
@@ -0,0 +1,54 @@
+﻿
+namespace BackgroundTask
+{
+    using System;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+    using Windows.ApplicationModel.Background;
+    using Windows.Storage;
+
+    public sealed class TestBackgroundTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral defferal;
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            this.defferal = taskInstance.GetDeferral();
+            taskInstance.Canceled += OnCanceled;
+
+            await ExecuteActionAsync();
+
+            this.Finish();
+        }
+
+        private async Task ExecuteActionAsync()
+        {
+            StorageFolder folder = null;
+            try
+            {
+                folder = await KnownFolders.DocumentsLibrary.CreateFolderAsync("SampleFolder", CreationCollisionOption.OpenIfExists);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+
+            if (folder != null)
+                await folder.CreateFileAsync("text.bgtxt", CreationCollisionOption.OpenIfExists);
+        }
+
+        private void OnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            this.Finish();
+        }
+
+        private void Finish()
+        {
+            if (this.defferal != null)
+            {
+                this.defferal.Complete();
+                this.defferal = null;
+            }
+        }
+    }
+}
---
+++ WpfBgTaskSample/WpfBgTaskSample/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.ApplicationModel.Background;
+
+namespace WpfBgTaskSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void RegisterBackgroundTasks(object sender, RoutedEventArgs e)
+        {
+            await RegisterBackgroundTasksAsync();
+        }
+
+        private async Task RegisterBackgroundTasksAsync()
+        {
+            await BackgroundExecutionManager.RequestAccessAsync();
+
+            var entryPoint = "BackgroundTask.TestBackgroundTask";
+            var condition = new SystemCondition(SystemConditionType.InternetAvailable);
+
+            // check with SYSTEM trigger (when internet available)
+            var internetAvailableTrigger = new SystemTrigger(SystemTriggerType.InternetAvailable, false);
+            RegisterBackgroundTask(entryPoint, "TestBackgroundTask", internetAvailableTrigger, null);
+
+            // check with Maintenance trigger (when internet available)
+            var maintenanceTriggerPeriod = TimeSpan.FromDays(1);
+            var timeTrigger = new MaintenanceTrigger((uint)maintenanceTriggerPeriod.TotalMinutes, false);
+            RegisterBackgroundTask(entryPoint, "TestBackgroundTask", timeTrigger, condition);
+        }
+
+        private void RegisterBackgroundTask(string entryPoint, string taskName, IBackgroundTrigger trigger, IBackgroundCondition condition)
+        {
+            var builder = new BackgroundTaskBuilder { Name = taskName, TaskEntryPoint = entryPoint };
+            builder.SetTrigger(trigger);
+
+            if (condition != null)
+            {
+                builder.AddCondition(condition);
+            }
+
+            try
+            {
+                builder.Register();
+            }
+            catch (Exception) { }
+        }
+    }
+}
